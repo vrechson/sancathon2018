48a6bd3d47a18c5a1e6f59dda59599437e0a7925
dinThea <iagoelifa@gmail.com>
2018-06-09 17:25:55 -0300

    Calculo de prioridade

--- getData.py
+++ getData.py
@@ -7,14 +7,27 @@ offset = 0
 
 def getData():
 
-    data = {'bpm':'NaN', 'motor':5, 'irpm':15}
+    data = {'bpm':'NaN', 'motor':1, 'irpm':15}
 
     data['bpm'] = getHeartRate.getBPM(heartURL, 20)
     data['altura'] = getHeight.getHeight(offset)
     
-    completeData = []
-    
-    return data
+    avalData = []
+    if (data['bpm'] > 140 or data['bpm'] < 60) :
+        avalData[0] = 5
+    else :
+        avalData[0] = (data['bpm'] - 60) * 5 / (140 - 60)
+    if (data['irpm'] > 35 or data['irpm'] < 10) :
+        avalData[1] = 5
+    else :
+        avalData[1] = (data['irpm'] - 10) * 5 / (35 - 10)
+    avalData[2] = data['motor']
+
+    soma = 0
+    for i in avalData :
+        soma += i
+    completeData = [data, soma/3]
+    return completeData
 
 data = getData()
-print(data['bpm'])
+print(data)
