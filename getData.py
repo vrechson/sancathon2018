f7f795304b5d5b6cbb60d7db4524edac05670018
dinThea <iagoelifa@gmail.com>
2018-06-09 20:42:59 -0300

    Database otimizado

--- getData.py
+++ getData.py
@@ -9,44 +9,42 @@ heartURL = 'http://172.20.84.78:8080/shot.jpg'
 frontCameraURL = []
 offset = 0
 
-def getData():
+def getData(label):
 
-    data = {'bpm':'NaN', 'motor':1, 'irpm':15}
-
-    time.sleep(5)
-    data['bpm'] = getHeartRate.getBPM(heartURL, 20)
+    if (label == 'bpm') :
+        return getHeartRate.getBPM(heartURL, 20)
+    elif (label == 'motor') :
+        return 1
+    elif (label == 'irpm') :
+        return 15
+    elif (label == 'altura') :
+        return getHeight.getHeight(offset)
+    elif (label == 'priority'):
+        return 0
     
-    time.sleep(5)
-    data['altura'] = getHeight.getHeight(offset)
-    
-    avalData = [1, 1, 1]
-    if (data['bpm'] > 140 or data['bpm'] < 60) :
-        avalData[0] = 5
-    else :
-        avalData[0] = (data['bpm'] - 60) * 5 / (140 - 60)
-    if (data['irpm'] > 35 or data['irpm'] < 10) :
-        avalData[1] = 5
-    else :
-        avalData[1] = (data['irpm'] - 10) * 5 / (35 - 10)
-    avalData[2] = data['motor']
+    # if (data['bpm'] > 140 or data['bpm'] < 60) :
+        # avalData[0] = 5
+    # else :
+        # avalData[0] = (data['bpm'] - 60) * 5 / (140 - 60)
+    # if (data['irpm'] > 35 or data['irpm'] < 10) :
+        # avalData[1] = 5
+    # else :
+        # avalData[1] = (data['irpm'] - 10) * 5 / (35 - 10)
+    # avalData[2] = data['motor']
 
-    soma = 0
-    for i in avalData :
-        soma += i
-    completeData = [data, soma/3]
-    return completeData
+    # soma = 0
+    # for i in avalData :
+        # soma += i
+    # completeData = [data, soma/3]
+    # return completeData
 
 while True :
     result = firebase.get('Pedidos', None)
+    print (result)
     if result is not None :
-        for i in result :
-            data = getData()
-            print (data[0])
-            val = i
-            data[0]['aval'] = data[1]
-            motor = data[0]['motor']
-            proc = {'processed' : 0}
-            result = firebase.post('Respostas/'+str(val['id']), data[0])
+        for i in result:
+            data = { i['label'] : getData(i['label']) }
+            result = firebase.post('Respostas/', data)
             firebase.delete('Pedidos', '0')
             print (result)
     
