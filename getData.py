06b4908132a4062d982ce405fc216397c89c165b
dinThea <iagoelifa@gmail.com>
2018-06-09 16:54:47 -0300

    Funcao de calculo geral

---
+++ getData.py
@@ -0,0 +1,17 @@
+import getHeight
+import getHeartRate
+
+heartURL = 'http://172.20.84.78:8080/shot.jpg'
+frontCameraURL = []
+offset = 0
+
+def getData():
+
+    data = {'bpm':'NaN', 'motor':5, 'irpm':15}
+
+    data['bpm'] = getHeartRate.getBPM(heartURL)
+    data['altura'] = getHeight.getHeight(offset)
+    
+    completeData = []
+    
+    return data
